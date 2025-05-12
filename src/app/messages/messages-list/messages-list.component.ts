005940b18e4fdb66852b7ecd968a573cb6d9e86e
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-12 15:51:46 +0200

    try triggering change detection without signals

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -12,6 +12,21 @@ export class MessagesListComponent {
   private messagesService = inject(MessagesService);
   messages = this.messagesService.allMessages
 
+
+  //private cdRef = inject(ChangeDetectorRef); 
+  //private destroyRef = inject(DestroyRef);
+
+  //messages: string[] = [];
+
+  //ngOnInit(): void {
+  //  this.messagesService.messages$.subscribe((messages) => {
+  //    this.messages = messages;
+  //    this.cdRef.markForCheck();
+  //  });
+  //  this.destroyRef.onDestroy(() => {
+  //    subscribe.unsubscribe();
+  //  });
+
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
